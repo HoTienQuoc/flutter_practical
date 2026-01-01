9ce40122ec22a2cbd551ba53ae0a833a0f5b872c
HoTienQuoc <hotienquoc0429@gmail.com>
2026-01-01 15:38:37 +0700

    Refactor main.dart to implement a Scaffold and GradientContainer for improved UI structure and aesthetics

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,5 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: const Text("Hello, Flutter!")));
+  //Note MaterialApp is root, Scaffold is layout widget, Row is widget
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 47, 5, 120),
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  // Constructor
+  // GradientContainer() {
+  //   // initialization work
+  // }
+  // GradientContainer({key}) : super(key: key);
+  const GradientContainer({super.key});
+
+  @override
+  // Called for you, automatically, by Flutter
+  // (when it renders the user interface)
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
