2383dcd7f7266697174aaf1b392a2dd477aff8ec
HoTienQuoc <hotienquoc0429@gmail.com>
2026-01-22 18:29:30 +0700

    Add dice images and enhance GradientContainer with dice rolling functionality

--- flutter_application/lib/gradient_container.dart
+++ flutter_application/lib/gradient_container.dart
@@ -10,24 +10,58 @@ class GradientContainer extends StatelessWidget {
   //   // initialization work
   // }
   // GradientContainer({key}) : super(key: key);
-  const GradientContainer({super.key});
+  GradientContainer({super.key, required this.colors}); // First constructor
+
+  GradientContainer.purple({
+    super.key,
+  }) //🧩 Đây là constructor có tên (named constructor)
+  : colors = const [Colors.deepPurple, Colors.indigo]; // Second constructor
+  final List<Color> colors;
+
+  var activeDiceImage = "assets/images/dice-1.png";
+
+  void rollDice() {
+    activeDiceImage = "assets/images/dice-2.png";
+    print("Changing Dice Image");
+  }
 
   @override
   // Called for you, automatically, by Flutter
   // (when it renders the user interface)
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
+          // colors: [
+          //   // Color.fromARGB(255, 26, 2, 80),
+          //   // Color.fromARGB(255, 45, 7, 98),
+          // ],
+          colors: colors,
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: Center(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            StyledText('Hello World!'),
+            SizedBox(height: 16), // khoảng cách giữa 2 dòng
+            Image.asset(activeDiceImage, width: 200),
+            SizedBox(height: 16),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                // padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(fontSize: 28),
+              ),
+              child: const Text('Roll Dice'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application/gradient_container.dart';
 
 void main() {
+  final colors = [
+    Color.fromARGB(255, 26, 2, 80),
+    Color.fromARGB(255, 45, 7, 98),
+  ];
   //Note MaterialApp is root, Scaffold is layout widget, Row is widget
   runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 47, 5, 120),
-        body: GradientContainer(),
+        body: GradientContainer.purple(),
       ),
     ),
   );
